44b1944987887494cc4d3d7ac7d954944fd76c23
joaomiguelvercesi26 <joaomiguelvercesi26@gmail.com>
2026-03-31 01:46:08 +0000

    Aula0330_ic - flaskcom template

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_templates
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    l = "Leitura: Temperatura"
+    return send_file('src/templates/index.html')
+
 
 @app.route("/contato")
 def contato():
